748933a3232cbb6cb3e7311fb8f5c22eb89c80f6
Olexii Korshunov <korshunov.olexiy@gmail.com>
2022-12-21 16:40:23 +0200

    Update alertsQT.py

--- alertsQT.py
+++ alertsQT.py
@@ -15,8 +15,7 @@ class Window(QtWidgets.QMainWindow):
 
     def geometry_rect(self) -> QtCore.QRect:
         rect = QtWidgets.QApplication.desktop().availableGeometry()
-        toolbar = 30
-        win_width, win_height = rect.width()//4, rect.height() - toolbar
+        win_width, win_height = rect.width()//4, rect.height() - 30
         x = rect.width() - win_width - 1
         return QtCore.QRect(x, toolbar, win_width, win_height)
 
@@ -24,30 +23,10 @@ class Window(QtWidgets.QMainWindow):
 if __name__ == "__main__":
     import sys
 
-    def callback_function(bool_res):
-        # if bool(bool_res):
-        QMessageBox.question(window, "Title", str(bool_res), QMessageBox.Yes)
-
-    def on_load_finished():
-        browser.page().runJavaScript('''
-        function GetOblast(findObl){
-            let mycls = document.getElementsByClassName('map-district hour-0 active  air-raid ');
-            for (let i = 0; i < mycls.length; i++){
-                if (mycls[i].attributes['data-oblast'].nodeValue.toLowerCase().includes(findObl.toLowerCase())){
-                    return true;
-                }
-            }
-            return false;
-        };
-        GetOblast('Сумська');
-        ''', callback_function)
-
     app = QtWidgets.QApplication(sys.argv)
     window = Window()
     browser = QWebEngineView(window)
     window.setCentralWidget(browser)
     url = QtCore.QUrl("https://alerts.in.ua")
     browser.load(url)
-    # QMessageBox.question(window, "Title", browser.page().findText("Сумська"), QMessageBox.Yes|QMessageBox.No)
-    browser.loadFinished.connect(on_load_finished)
     sys.exit(app.exec_())
