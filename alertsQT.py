2fe558614476535e34924211811faf5a9a52b990
Olexii Korshunov <korshunov.olexiy@gmail.com>
2022-12-22 16:37:53 +0200

    Update alertsQT.py

--- alertsQT.py
+++ alertsQT.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import json
+from pathlib import Path
 from time import sleep
 from urllib.request import urlopen
-from io import StringIO
-
 
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QMessageBox
 
@@ -15,20 +15,25 @@ from PyQt5.QtWidgets import QMessageBox
 class Worker(QObject):
     finished = pyqtSignal()
     alarm_on = pyqtSignal(int)
+    alarmed = False
 
     def __init__(self, oblast) -> None:
         super().__init__()
         self.oblast = oblast.lower()
 
-    def run(self):
-        with urlopen('https://sirens.in.ua/api/v1/', timeout=10) as response:
-            sleep(3)
-            data_raw = json.loads(response.read())
-            data = data_raw[self.oblast].lower()
-            if data[self.oblast]:
-                self.alarm_on.emit(1)
-            return json.loads(data)
-        # self.finished.emit()
+    def run(self) -> None:
+        while True:
+            with urlopen('https://sirens.in.ua/api/v1/', timeout=10) as response:
+                sleep(3)
+                data_raw = json.loads(response.read())
+                data = {k.lower(): v for k, v in data_raw.items()}
+                data_oblast = data[self.oblast.lower()]
+                if not data_oblast is None and not self.alarmed:
+                    self.alarm_on.emit(True)
+                    self.alarmed = True
+                if data_oblast is None:
+                    self.alarm_on.emit(False)
+                    self.alarmed = False
 
 
 class Window(QtWidgets.QMainWindow):
@@ -54,11 +59,20 @@ class Window(QtWidgets.QMainWindow):
         self.worker.moveToThread(self.thread)
         self.thread.started.connect(self.worker.run)
         self.worker.finished.connect(self.worker.deleteLater)
-        # self.worker.alarm_on.connect(self.ShowMessage)
+        self.worker.alarm_on.connect(self.ShowMessage)
         self.thread.start()
 
     def ShowMessage(self):
-        QMessageBox.question(window, "Title", f"msg", QMessageBox.Yes)
+        msgbox = QMessageBox(parent=self)
+        msgbox.setIconPixmap(QPixmap(str(Path().cwd().joinpath("msg_icon.png"))))
+        msgbox.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
+        msgbox.setWindowTitle("Повітряна тривога")
+        msgbox.setDefaultButton(QMessageBox.Ok)
+        if self.worker.alarmed: # if alarm on
+            msgbox.setText("УВАГА! Повітряна тривога! ВСІ В УКРИТТЯ!!!")
+        else: # if alarm off
+            msgbox.setText("ВІДБІЙ ПОВІТРЯНОЇ ТРИВОГИ")
+        msgbox.exec_()
 
 
 if __name__ == "__main__":
@@ -69,10 +83,11 @@ if __name__ == "__main__":
     browser = QWebEngineView(window)
     window.setCentralWidget(browser)
     window.check_alarm()
-    url = QtCore.QUrl("https://alerts.in.ua")
+    url = QtCore.QUrl("https://alerts.in.ua/lite")
     browser.load(url)
     # QMessageBox.question(window, "Title", browser.page().findText("Сумська"), QMessageBox.Yes|QMessageBox.No)
     ret = app.exec_()
-    window.thread.quit()
-    window.thread.wait()
+    window.thread.terminate()
+    # window.thread.quit()
+    # window.thread.wait()
     sys.exit(ret)
